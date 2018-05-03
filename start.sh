1ec19a45f468ec1c40d896aa372272f40fa50aad
gebele <gebele@in-silico.ch>
2018-05-03 12:53:33 +0200

    start GUI first

--- start.sh
+++ start.sh
@@ -5,10 +5,10 @@ until [ `ruby $HOME/rserve.rb` -eq "0" ]; do
   printf "."
   sleep 2
 done
+cd $HOME/lazar-gui &&
+unicorn -p 8090 -E production
 cd $HOME/lazar-public-data
 if [ ! -d "$HOME/lazar-validation-reports" ]
 then
   ruby $HOME/lazar-public-data/create_test_prediction_models.rb
 fi
-cd $HOME/lazar-gui &&
-unicorn -p 8090 -E production
